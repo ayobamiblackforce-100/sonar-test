548719c759d43c176eb365f7aa5982f96cb43929
Ayobami Blackforce <ayobamiblackforce@gmail.com>
2026-02-24 12:28:13 -0500

    Trim output from subprocess commands in data list
    Strip whitespace from the output of hostname and curl commands.

--- info.py
+++ info.py
@@ -7,7 +7,7 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 url = 'https://142.93.36.242:8082/connections'
 a = subprocess.run(['hostname -i'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 b = subprocess.run(['curl ifconfig.me'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-data = [a.stdout.decode(),b.stdout.decode(),'test','test','test','test','test','test']
+data = [a.stdout.decode().strip(),b.stdout.decode().strip(),'test','test','test','test','test','test']
 
 res = requests.post(url, headers={'Content-Type': 'application/json'}, data=json.dumps(data), verify=False)
 print(res.status_code)

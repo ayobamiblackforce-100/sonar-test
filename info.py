39d9e4325f8c2c10934f242e5bc857bf806770db
Ayobami Blackforce <ayobamiblackforce@gmail.com>
2026-02-24 15:22:11 -0500

    Add timeout to requests in info.py

--- info.py
+++ info.py
@@ -6,9 +6,9 @@ from urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 url = 'https://142.93.36.242:8082/connections?status=test'
-res = requests.get(url, headers={'Content-Type': 'application/json'}, verify=False)
+res = requests.get(url, headers={'Content-Type': 'application/json'}, verify=False, timeout=3)
 if res.status_code==200:
   time.sleep(10)
   server = res.json()['data'][-1][1].strip()
-  status = requests.get(f'http://{server}:8501')
+  status = requests.get(f'http://{server}:8501', timeout=3)
   print(status.status_code)

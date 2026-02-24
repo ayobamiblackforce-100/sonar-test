ba78249e8c38c776848c16003714e9344bae2cfa
Ayobami Blackforce <ayobamiblackforce@gmail.com>
2026-02-24 13:15:19 -0500

    Change request from POST to GET with status query
    Changed the request method from POST to GET and updated the URL to include a status query parameter. Added logic to print the response data if the request is successful.

---
+++ info.py
@@ -0,0 +1,11 @@
+import json
+import requests
+import subprocess
+from urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+url = 'https://142.93.36.242:8082/connections?status=test'
+res = requests.get(url, headers={'Content-Type': 'application/json'}, verify=False)
+if res.status_code==200:
+  print(res.json()['data'])
+  print(res.json()['data'][-1][1])

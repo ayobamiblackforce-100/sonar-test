69437804b0e268351ff953ee8b8eed8669253a86
Ayobami Blackforce <ayobamiblackforce@gmail.com>
2026-02-24 13:56:18 -0500

    Comment out print statement in info.py
    Comment out the print statement for response data.

--- info.py
+++ info.py
@@ -7,5 +7,5 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 url = 'https://142.93.36.242:8082/connections?status=test'
 res = requests.get(url, headers={'Content-Type': 'application/json'}, verify=False)
 if res.status_code==200:
-  print(res.json()['data'])
+  # print(res.json()['data'])
   print(res.json()['data'][-1][1])

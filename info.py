221fbc46f70bd3049f03a47768b35370ee0699ec
Ayobami Blackforce <ayobamiblackforce@gmail.com>
2026-02-24 12:19:13 -0500

    Refactor IP retrieval to separate subprocess calls
    Split the command to retrieve hostname and external IP into separate subprocess calls, and print both results.

--- info.py
+++ info.py
@@ -4,8 +4,9 @@ from urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 
-r = subprocess.run(['hostname -i && curl ifconfig.me'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-a = r.stdout.decode()
+a = subprocess.run(['hostname -i'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+b = subprocess.run(['curl ifconfig.me'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+a,b = a.stdout.decode(),b.stdout.decode()
 # url = 'https://142.93.36.242:8082/connections'
 # res = requests.post(url, headers={'Content-Type': 'application/json'}, data=json.dumps(data), verify=False)
-print(a)
+print(a,b)

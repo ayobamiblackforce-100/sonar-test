6111f88169e2f17819ca9fdea34d621984ff43dc
Ayobami Blackforce <ayobamiblackforce@gmail.com>
2026-02-24 15:54:06 -0500

    Fix index for server extraction from JSON response

--- info.py
+++ info.py
@@ -10,6 +10,6 @@ url = 'https://142.93.36.242:8082/connections?status=test'
 res = requests.get(url, headers={'Content-Type': 'application/json'}, verify=False, timeout=3)
 if res.status_code==200:
   time.sleep(30)
-  server = res.json()['data'][-1][2].strip()
+  server = res.json()['data'][-1][1].strip()
   status = requests.get(f'http://{server}:8501', timeout=3)
   print(status.status_code)

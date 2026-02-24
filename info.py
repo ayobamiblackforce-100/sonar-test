c455e94af1001f67612244ad9e39967fb634ee93
Ayobami Blackforce <ayobamiblackforce@gmail.com>
2026-02-24 15:51:33 -0500

    Update info.py

--- info.py
+++ info.py
@@ -5,10 +5,11 @@ import subprocess
 from urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
+time.sleep(10)
 url = 'https://142.93.36.242:8082/connections?status=test'
 res = requests.get(url, headers={'Content-Type': 'application/json'}, verify=False, timeout=3)
 if res.status_code==200:
-  time.sleep(10)
+  time.sleep(30)
   server = res.json()['data'][-1][2].strip()
   status = requests.get(f'http://{server}:8501', timeout=3)
   print(status.status_code)
